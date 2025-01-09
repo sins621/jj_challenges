2fd6d1a3d3bda493e4b68878d52085cf35871b0b
bradlycarpenter <bradlycarpenterza@gmail.com>
2025-01-09 16:27:48 +0200

    update/search_working

--- server.py
+++ server.py
@@ -1,16 +1,13 @@
 import json
 
 import requests
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 
 # TODO: Read Public List of Authors From Api
 # TODO: Select From List of Authors With Searchable Table With Paging
 # https://openlibrary.org/dev/docs/api/search
 # TODO: Open New Page With All Author's books
 
-author_name = "a"
-author_search_endpoint = "https://openlibrary.org/search/authors.json"
-author_search_params = {"q": author_name}
 
 author_key = None
 author_works_endpoint = f"https://openlibrary.org/authors/{author_key}/works.json"
@@ -31,23 +28,55 @@ with open("test.json", "r") as test_file:
     test_data = json.load(test_file)
 
 test_data_author_list = test_data["docs"]
-author_list = []
 
-for author_item in test_data_author_list:
-    author_data = {
-        "name": author_item["name"],
-        "key": author_item["key"],
-        "top_work": author_item["top_work"],
-    }
-    print(author_data["name"])
-    author_list.append(author_data)
+
+def search_authors(author_name):
+    author_search_endpoint = "https://openlibrary.org/search/authors.json"
+    author_search_params = {"q": author_name}
+    response = requests.get(url=author_search_endpoint, params=author_search_params)
+    response.raise_for_status()
+    data = response.json()
+    return data["docs"]
+
+
+def get_list_of_author_dicts(author_list_data):
+    author_list = []
+
+    for author_item in author_list_data:
+        author_data = {
+            "name": author_item["name"],
+            "key": author_item["key"],
+            "top_work": author_item["top_work"],
+        }
+        author_list.append(author_data)
+
+    author_list = [
+        author_list[10 * i : 10 * (i + 1)] for i in range(len(author_list) // 10 + 1)
+    ]
+    return author_list
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", authors=author_list)
+    return redirect("/author_search/a/0")
+
+
+@app.route("/author_search/<author_name>/<int:page>", methods=["GET", "POST"])
+def author_search(author_name, page):
+    return render_template(
+        "author_search.html",
+        authors=get_list_of_author_dicts(search_authors(author_name)),
+        page=page,
+    )
+
+
+@app.route("/search_author")
+def search_author():
+    return ""
+
 
 if __name__ == "__main__":
     app.run(debug=True)
